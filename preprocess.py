79646ba65f1f3759485fb5b3a7e1412765fcb118
Aarsh <aarshpatel2101@gmail.com>
2024-03-30 15:21:07 -0500

    add preprocess file

---
+++ preprocess.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('essays_dataset/index.csv', sep=';')
+
+for filename in df['filename']:
+    with open('essays_dataset/essays/' + filename, 'r') as file:
+        essay = file.read()
+
+        df.loc[df['filename'] == filename, 'essay'] = essay
+
+df.to_csv('essays_dataset/preprocessed_index.csv', index=False)
